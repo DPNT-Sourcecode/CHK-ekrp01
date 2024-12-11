b82ed4c8f454d982e6db5bd34d6974914bd97f87
sbx_user1051 <sbx_user1051@169.254.15.26>
2024-12-11 14:11:32 +0000

    Wed Dec 11 14:11:32 UTC 2024

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -1,7 +1,21 @@
+from typing import Any
+
+import pytest
 from solutions.CHK import checkout_solution
 
 
-def test_checkout() -> None:
-    result = checkout_solution.checkout("A")
+@pytest.mark.parametrize(
+    ("skus", "expected"),
+    [
+        ("1", -1),
+        (1, -1),
+        ("-", -1),
+        (None, -1),
+        ("A1B", -1),
+    ],
+)
+def test_checkout_invalid(skus: Any, expected: int) -> None:
+    result = checkout_solution.checkout(skus)
 
-    assert result is None
+    assert result == -1
+
